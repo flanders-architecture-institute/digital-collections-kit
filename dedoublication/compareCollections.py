df336326984fbb9c3b357c0d8822e3388adb5d7f
WimLo <47601554+WimLo@users.noreply.github.com>
2019-05-21 09:47:07 +0200

    Update compareCollections.py

--- dedoublication/compareCollections.py
+++ dedoublication/compareCollections.py
@@ -1,9 +1,39 @@
-subjectdf = pd.DataFrame('the content resource')
-testcorp = pd.read_excel('the content resource')
+import pandas as pd
+import re
 
-# Onderstaande code checkt op basis van checksums hoe vaak de bestanden in subjectdf voorkomen in testcorp.
+# Onderstaande code checkt op basis van checksums hoe vaak de bestanden in subjectdf voorkomen in testcorp en geeft statistieken per subfolder in een gekozen folder
+# Deze code gaat uit van een subjectdataframe dat resulteert uit het inlezen van een PREMIS-XML volgens onderzoek van Henk Vanstappen uit 2017.
 
-newdf = pd.merge(subjectdf, testcorp, on='messageDigest', how='left') # Er wordt een join gecreëerd - dit kan bij grote excels erg groot worden!!
-doubledf = newdf.dropna() # Dit geeft een gigaframe met per bestand het de bestanden uit testcorp waarvoor er een match bestaat. Dit naar een Excel brengen duurt erg lang.
-resultdf = doubledf['contentLocationValue'].value_counts() # Een nieuw datframe wordt gemaakt met per folderpath het aantal keer dat het voorkomt.
-resultdf.to_excel('C:\\Users\\Wim Lo\\Desktop\\CK\\resultdf.xlsx')
+subjectdf = pd.DataFrame('the content resource') # Kan komen uit een Json, een Premis-XML (zie hiervoor code), een Excel etc.
+testcorp = pd.read_excel('the content resource') # Kan komen uit een Json, een Premis-XML (zie hiervoor code), een Excel etc.
+
+# Stap 1: De gebruiker geeft aan in welke locatie hij de folders wilt onderzoeken
+
+researchloc = input('Welke xlink wil je onderzoeken? ') 
+subjectdf = subjectdf[subjectdf['contentLocationValue'].str.match(researchloc)] # str.match zoekt alle strings die starten met een gegeven string
+
+# Stap 2: We zoeken hoe vaak een bestand uit de subjectdf voorkomt in het testcorp (a.h.v. MD5-checksum)
+
+resdf = pd.merge(subjectdf, testcorp, on='messageDigest', how='left') # Er wordt een join gecreëerd - dit kan bij grote excels erg groot worden!!
+resdf['doubleCount'] = resdf.groupby('contentLocationValue')['contentLocationValue'].transform('count') # Dit geeft een gigaframe met per bestand het de bestanden uit testcorp waarvoor er een match bestaat. Dit naar een Excel brengen duurt erg lang.
+resdf = resdf.groupby(['contentLocationValue', 'objectIdentifierValue', 'originalName', 'messageDigest', 'doubleCount'])['filename'].agg('count').reset_index()
+resdf = resdf.drop(columns=['filename']) # Ziet er allemaal niet proper gecodeerd uit, maar het werkt
+
+# Stap 3: We maken een kolom 'uniek', 'nietUniek'
+def uniek(cellValue):
+    if cellValue > 1:
+        return "notUnique"
+    else:
+        return "Unique"
+
+resdf['uniqueOnDisk'] = resdf['doubleCount'].apply(uniek)
+
+# Stap 4: We creëren een extra kolom met de folders die voorwerp zijn van onderzoek (één niveau onder researchloc)
+
+def folder(cellValue):
+    cellValue = re.sub('^' + researchloc + '/', '', cellValue)
+    cellValue = cellValue.split('/')[0] # We splitsen het resterende path op en nemen de eerste waarde van de resulterende lijst
+    return cellValue
+
+resdf['researchedFolder'] = resdf['contentLocationValue'].apply(folder)
+resdf
