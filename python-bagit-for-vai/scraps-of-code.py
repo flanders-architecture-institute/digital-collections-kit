a182ab284e572e35a15b3c6fb6b71e6951bb3af7
WimLo <47601554+WimLo@users.noreply.github.com>
2020-04-03 19:52:05 +0200

    Create scraps-of-code.py

---
+++ python-bagit-for-vai/scraps-of-code.py
@@ -0,0 +1,3 @@
+# Heb je Anaconda geïnstalleerd, dan zit de bagit-module daar al in.
+# Vanuit een Python-interpreter van de map "Gent_Tunnel" op mijn Desktop een bag maken.
+python -m bagit --contact-name 'WimLo' "c://Users//Wim Lo//Desktop//Gent_Tunnel"
