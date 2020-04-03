e4490f00a72e2679541a7cf0f5701367ad26b2e9
WimLo <47601554+WimLo@users.noreply.github.com>
2020-04-03 20:21:15 +0200

    Update scraps-of-code.py

--- python-bagit-for-vai/scraps-of-code.py
+++ python-bagit-for-vai/scraps-of-code.py
@@ -1,3 +1,19 @@
 # Heb je Anaconda geïnstalleerd, dan zit de bagit-module daar al in.
 # Vanuit een Python-interpreter van de map "Gent_Tunnel" op mijn Desktop een bag maken.
+# Contactnaam Wim Lo komt in de bag-info terecht.
 python -m bagit --contact-name 'WimLo' "c://Users//Wim Lo//Desktop//Gent_Tunnel"
+
+python -m bagit --contact-name --md5 'WimLo' "c://Users//Wim Lo//Desktop//Gent_Tunnel" #Enkel md5
+
+# Een volledige validatie.
+python -m bagit --validate "c://Users//Wim Lo//Desktop//Gent_Tunnel"
+
+# Een snelle validatie (geen checksums, maar wel structuur en bestandsgrootte)
+python -m bagit --validate --fast "c://Users//Wim Lo//Desktop//Gent_Tunnel"
+
+
+# Using BagIt from your Python code:
+
+    import bagit
+    bag = bagit.make_bag('example-directory', {'Contact-Name': 'Ed Summers'})
+    print(bag.entries)
